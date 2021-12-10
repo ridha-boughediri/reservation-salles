db00fe4432bbacda864d471b2f262d456508896c
leo-capdeillayre <leo.capdeillayre@laplateforme.io>
2021-12-10 13:25:24 +0100

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -1,9 +1,26 @@
 <?php
+session_start();
+$bdd = new PDO('mysql:host=localhost ;dbname=reservationsalles', 'root', '');
+
+if (isset($_POST['signin'])) {
+    $login = htmlspecialchars($_POST['login']);
+    $mail = htmlspecialchars($_POST['mail']);
+    $password = sha1($_POST['password']);
+    $confpassword = sha1($_POST['confpassword']);
+    if (!empty($_POST['login']) and !empty($_POST['mail']) and !empty($_POST['password'])) {
+        if ($password == $confpassword) {
+            # code...
+        }
+    }
+}
+
+
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,11 +28,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
     <div id="login">
         <h3 class="text-center text-white pt-5">Login form</h3>
         <div class="container">
@@ -25,28 +43,24 @@
                         <form id="login-form" class="form" action="" method="post">
                             <h3 class="text-center text-info">Inscription</h3>
                             <div class="form-group">
-                                <label for="username" class="text-info">name:</label><br>
-                                <input type="text" name="username" id="username" class="form-control">
+                                <label for="username" class="text-info">Login:</label><br>
+                                <input type="text" name="login" id="username" class="form-control">
                             </div>
                             <div class="form-group">
-                                <label for="username" class="text-info">Username:</label><br>
-                                <input type="text" name="username" id="username" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label for="username" class="text-info">MAIL:</label><br>
-                                <input type="text" name="username" id="username" class="form-control">
+                                <label for="username" class="text-info">Mail:</label><br>
+                                <input type="text" name="mail" id="username" class="form-control">
                             </div>
                             <div class="form-group">
                                 <label for="password" class="text-info">Password:</label><br>
                                 <input type="text" name="password" id="password" class="form-control">
                             </div>
                             <div class="form-group">
-                                <label for="password" class="text-info">REPassword:</label><br>
-                                <input type="text" name="password" id="password" class="form-control">
+                                <label for="password" class="text-info">RePassword:</label><br>
+                                <input type="text" name="confpassword" id="password" class="form-control">
                             </div>
                             <div class="form-group">
                                 <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
-                                <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
+                                <input type="submit" name="signin" class="btn btn-info btn-md" value="submit">
                             </div>
                             <div id="register-link" class="text-right">
                                 <a href="#" class="text-info">Register here</a>
