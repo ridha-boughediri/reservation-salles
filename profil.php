e5e0e03247fe02a49c5da12f7d3c6d28c2eda8b3
leo-capdeillayre <leo.capdeillayre@laplateforme.io>
2021-12-13 12:25:31 +0100

    Update profil.php

--- profil.php
+++ profil.php
@@ -3,131 +3,78 @@
 include('./fileconfig/config.php');
 include('./fileconfig/configuser.php');
 
+$getid = $_SESSION['id'];
+
+if (isset($_POST['edit'])) {
+    if (!empty($_POST['login'])) {
+        $login = htmlspecialchars($_POST['login']);
+        $recupLogin = $bdd->prepare('UPDATE utilisateurs SET login = ? WHERE id = ?');
+        $recupLogin->execute(array($login, $getid));
+    }
+}
+if (isset($_POST['edit'])) {
+    if (!empty($_POST['mail'])) {
+        $mail = htmlspecialchars($_POST['mail']);
+        $recupMail = $bdd->prepare('UPDATE utilisateurs SET mail = ? WHERE id = ?');
+        $reucpMail->execute(array($mail, $getid));
+    }
+}
+if (isset($_POST['edit'])) {
+    if (!empty($_POST['password'])) {
+        $password = sha1($_POST['password']);
+        $recupPassword = $bdd->prepare('UPDATE utilisateurs SET password = ? WHERE id = ?');
+        $recupPassword->execute(array($password, $getid));
+    }
+}
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="/css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Page profil</title>
 </head>
+
 <body>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
     <div id="login">
         <h3 class="text-center text-white pt-5">Login form</h3>
         <div class="container">
-
+            <?php
+            $recupUsers = $bdd->prepare('SELECT * FROM utilisateurs WHERE id = ?');
+            $recupUsers->execute(array($getid));
+            $recupUserInfo = $recupUsers->fetch();
+            ?>
             <div id="login-row" class="row justify-content-center align-items-center">
                 <div id="login-column" class="col-md-6">
                     <div id="login-box" class="col-md-12">
                         <form id="login-form" class="form" action="" method="post">
                             <h3 class="text-center text-info">information profil</h3>
                             <div class="form-group">
-                                <label for="username" class="text-info">name:</label><br>
-                                <input type="text" name="username" id="username" class="form-control">
+                                <label for="username" class="text-info">Login:</label><br>
+                                <input type="text" name="login" placeholder="<?php echo $recupUserInfo['login'] ?>">
                             </div>
                             <div class="form-group">
-                                <label for="username" class="text-info">Username:</label><br>
-                                <input type="text" name="username" id="username" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label for="username" class="text-info">MAIL:</label><br>
-                                <input type="text" name="username" id="username" class="form-control">
+                                <label for="username" class="text-info">Mail:</label><br>
+                                <input type="text" name="mail" placeholder="<?php echo $recupUserInfo['mail'] ?>">
                             </div>
                             <div class="form-group">
                                 <label for="password" class="text-info">Password:</label><br>
-                                <input type="text" name="password" id="password" class="form-control">
+                                <input type="text" name="password" placeholder="<?php echo $recupUserInfo['password'] ?>">
                             </div>
-                            <div class="form-group">
-                                <label for="password" class="text-info">REPassword:</label><br>
-                                <input type="text" name="password" id="password" class="form-control">
+                            <div>
+                            <input style="color: green;" type="submit" name="edit" value="Edit">
                             </div>
-                            <div class="form-group">
-                                <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
-                                <input type="submit" name="submit" class="btn btn-info btn-md" value="modifier">
-                            </div>
-                           
                         </form>
                     </div>
                 </div>
-                <div class="row">
-		<div class="span12">
-    	    <table class="table-condensed table-bordered table-striped">
-                <thead>
-                    <tr>
-                      <th colspan="7" >
-                        <a class="btn"><i class="icon-chevron-left"></i></a>
-                        <a class="btn">February 2012</a>
-                        <a class="btn"><i class="icon-chevron-right"></i></a>
-                      </th>
-                    </tr>
-                    <tr>
-                        <th>Su</th>
-                        <th>Mo</th>
-                        <th>Tu</th>
-                        <th>We</th>
-                        <th>Th</th>
-                        <th>Fr</th>
-                        <th>Sa</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td class="muted">29</td>
-                        <td class="muted">30</td>
-                        <td class="muted">31</td>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                        <td>8</td>
-                        <td>9</td>
-                        <td>10</td>
-                        <td>11</td>
-                    </tr>
-                    <tr>
-                        <td>12</td>
-                        <td>13</td>
-                        <td>14</td>
-                        <td>15</td>
-                        <td>16</td>
-                        <td>17</td>
-                        <td>18</td>
-                    </tr>
-                    <tr>
-                        <td>19</td>
-                        <td><strong>20</strong></td>
-                        <td>21</td>
-                        <td>22</td>
-                        <td>23</td>
-                        <td>24</td>
-                        <td>25</td>
-                    </tr>
-                    <tr>
-                        <td>26</td>
-                        <td>27</td>
-                        <td>28</td>
-                        <td>29</td>
-                        <td class="muted">1</td>
-                        <td class="muted">2</td>
-                        <td class="muted">3</td>
-                    </tr>
-                </tbody>
-            </table>    
-		</div>
-	</div>
-</div>
             </div>
         </div>
     </div>
