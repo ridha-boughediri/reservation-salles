96c51c283a6882b90ae260de147fcd3ea8a6233e
Samir-Belhadj <91877377+Samir-Belhadj@users.noreply.github.com>
2022-01-13 13:30:27 +0100

    Update reservationsalles.sql

--- reservationsalles.sql
+++ reservationsalles.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 04 jan. 2022 à 09:27
+-- Généré le : jeu. 13 jan. 2022 à 12:29
 -- Version du serveur : 8.0.27
 -- Version de PHP : 7.4.26
 
@@ -61,19 +61,7 @@ CREATE TABLE IF NOT EXISTS `reservations` (
   `fin` datetime NOT NULL,
   `id_utilisateur` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
-
---
--- Déchargement des données de la table `reservations`
---
-
-INSERT INTO `reservations` (`id`, `titre`, `description`, `debut`, `fin`, `id_utilisateur`) VALUES
-(6, 'laroyale', 'sejour dans la suite parental', '2022-01-04 11:00:00', '2022-01-04 14:00:00', 1),
-(7, 'lapdeluxe', 'sejour pour nos noces', '2021-12-24 11:00:00', '2021-12-25 19:00:00', 1),
-(8, 'laexecut', 'sejour dans la suite parental', '2021-12-16 12:00:00', '2021-12-18 14:00:00', 1),
-(9, 'laroyale', 'sejour dans la suite parental', '2021-12-29 12:00:00', '2021-12-31 14:00:00', 1),
-(10, 'lapdeluxe', 'tout est gratuit', '2021-12-01 13:27:00', '2021-12-04 13:28:00', 1),
-(11, 'laexecut', 'c\'est super genial', '2021-12-17 13:29:00', '2021-12-02 13:29:00', 1);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -88,14 +76,7 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `password` varchar(255) NOT NULL,
   `mail` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
-
---
--- Déchargement des données de la table `utilisateurs`
---
-
-INSERT INTO `utilisateurs` (`id`, `login`, `password`, `mail`) VALUES
-(1, 'Samir-Gonzalez', 'c045c9838ba93fe23b0842c44195bfbd7298d35d', 's@s.s');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
